f97cad177e2fcb46502ec3702466f0760d9010b3
dmc <david.mark.clements@gmail.com>
2025-07-15 17:20:04 +0200

    errors fix

--- errors.js
+++ errors.js
@@ -7,19 +7,17 @@ class PearError extends require('pear-api/errors') {
 }
 
 function ERR_HTTP_GONE () {
-  const err = new PearError('Gone', 'ERR_HTTP_GONE', ERR_HTTP_GONE)
-  err.status = 410
-  return err
+  return new PearError('Gone', ERR_HTTP_GONE, { status: 410 })
 }
 
 function ERR_HTTP_BAD_REQUEST (msg = 'Bad Request') {
-  const err = new PearError(msg, 'ERR_HTTP_BAD_REQUEST', ERR_HTTP_BAD_REQUEST)
+  const err = new PearError(msg, ERR_HTTP_BAD_REQUEST, { status: 400 })
   err.status = 400
   return err
 }
 
 function ERR_HTTP_NOT_FOUND (msg) {
-  const err = new PearError(msg, 'ERR_HTTP_NOT_FOUND', ERR_HTTP_NOT_FOUND)
+  const err = new PearError(msg, ERR_HTTP_NOT_FOUND, { status: 404 })
   err.status = 404
   return err
 }
