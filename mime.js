65c7079b86803b0c0a06c326c4fdfc4dadb531c8
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-08-13 03:44:34 +0800

    Use get-mime-type in mime.js

--- mime.js
+++ mime.js
@@ -1,25 +1,13 @@
 'use strict'
-const db = require('mime-db')
+const getMimeType = require('get-mime-type')
 
-module.exports = class Mime {
-  constructor () {
-    this.extmap = new Map()
-    for (const [name, type] of Object.entries(db)) {
-      if (!type.extensions || !type.extensions.length) continue
-      for (const extension of type.extensions) {
-        this.extmap.set(extension, name)
-      }
-    }
-  }
+module.exports = function getType (filepath) {
+  let contentType = getMimeType(filepath.split('.').pop() || 'js')
 
-  type (filepath) {
-    const split = filepath.split('.')
-    let contentType = this.extmap.get(split.pop() || 'js')
-    if (!contentType) return 'application/octet-stream'
-    contentType = contentType.replace('application/node', 'application/javascript').replace('text/javascript', 'application/javascript')
-    if (contentType === 'application/javascript' || contentType === 'text/html' || contentType === 'application/json') {
-      contentType += '; charset=utf-8'
-    }
-    return contentType
-  }
+  if (!contentType) return 'application/octet-stream'
+
+  contentType = contentType.replace('application/node', 'application/javascript').replace('text/javascript', 'application/javascript')
+  if (contentType === 'application/javascript' || contentType === 'text/html') contentType += '; charset=utf-8'
+
+  return contentType
 }
